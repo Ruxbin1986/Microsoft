c2f4d77524c64b60b13d9a21c6568420a1050566
Ted B <breedon.ted@gmail.com>
2016-01-07 10:38:37 -0600

    Update AzureVMAutoShutdown.ps1

--- AzureVMAutoShutdown.ps1
+++ AzureVMAutoShutdown.ps1
@@ -3,15 +3,18 @@ workflow Stop-VM {
 		$startTime = get-date
 		$cred = Get-AutomationPSCredential -Name "AutoCred"
 		$throwAway = Login-AzureRMAccount -Credential $cred
-		$subscription = "RUXBIN"
+		$subscriptionname = "Ruxbin"
+		#$suboutput = Select-AzureRMSubscription -SubscriptionName $subscriptionname
+		#$subscriptionoutput = Get-AzureRmSubscription | fl SubscriptionName | Out-String
 		$results = Get-AzureRmVM | Stop-AzureRmVM -force | Out-String
 		if($results -eq $null)
     		{
-        		Write-Output "There are no VMs to stop"
+        		Write-Output "There are no VMs to stop in $subscriptionoutput"
     		}
     		else
     		{
-			$subject = "Azure Automation - VMs deallocated from " + $subscription #" in " + ([DateTime]::Now - $startTime).ToString()
+    			$elapsedtime = (((Get-Date) - $startTime).Seconds).ToString()
+			$subject = "Azure Automation - VMs deallocated from " + $subscriptionoutput + " in " + $elapsedtime + " Seconds"
 			$cred = Get-AutomationPSCredential -Name "EmailCredential"
 			Send-MailMessage -To "v-tebree@microsoft.com" -Subject $subject -Body $results -Port 587 -SmtpServer smtp.live.com -Credential $cred -UseSsl -From $cred.Username
  		}
