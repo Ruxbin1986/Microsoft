75ea8ab4b48eeb5f7129ef568f1a70b41a8d1ca5
Ted B <breedon.ted@gmail.com>
2016-01-31 21:28:10 -0600

    Update QuickWindows12R2Create.ps1

--- QuickWindows12R2Create.ps1
+++ QuickWindows12R2Create.ps1
@@ -1,24 +1,19 @@
-#Quickly Creates a Windows Server 2012 R2 Datacenter Virtual Machine
-$cloudservice = Read-host -Prompt "Enter existing Cloud Service Name"
-#Virtual Machine Name
-$vmname = Read-Host -Prompt "Enter Azure Virtual Machine Name"
-#Virtual Machine Size; Medium Recommended
-$vmsize = Read-Host -Prompt "Enter Virtual Machine Size"
-#Local Machine Username
-$localuser = Read-Host -Prompt "Enter local Username"
-#Local Machine Username Password
-$localuserpw = Read-Host -Prompt "Enter local user password" -AsSecureString
-#Domain Name
-$domain = "xxxx"
-#Complete FQDN
-$fqdn = "xxxxx"
-#Domain Administrator
-$domainadmin = Read-Host -Prompt "Enter username that has permisions to add the machine to the domain... or something"
-#Domain Administrator Password
-$domainadminpw = Read-Host -Prompt "Enter domain username's password" -AsSecureString
-#Selects the most recent Azure Windows 2012 R2 VM Image
-$vmimage = Get-AzureVMImage | Where { $_.ImageFamily -eq "Windows Server 2012 R2 Datacenter" } | sort PublishedDate -Descending | select -ExpandProperty ImageName -First 1
-#Creates a new Azure Virtual Machine Configuration
-$vm = New-AzureVMConfig -Name $vmname -InstanceSize $vmsize -ImageName $vmimage
-#Adds a provisioning configuration to the VM; joining the Domain
-$vm | Add-AzureProvisioningConfig -AdminUsername $localuser -WindowsDomain –Password $localuserpw -JoinDomain $fqdn -Domain $domain -DomainUserName $domainadmin -DomainPassword $domainadminpw | New-AzureVM -ServiceName $cloudservice
+$family="Windows Server 2012 R2 Datacenter"
+$image=Get-AzureVMImage | where { $_.ImageFamily -eq $family } | sort PublishedDate -Descending | select -ExpandProperty ImageName -First 1
+$vmname= Read-Host -Prompt "Virtual Machine Name"
+$vmsize= Read-Host -Prompt "Virtual Machine Size"
+$vm1=New-AzureVMConfig -Name $vmname -InstanceSize $vmsize -ImageName $image
+$cred1=Get-Credential –Message "Type the name and password of the local administrator account."
+$cred2=Get-Credential –Message "Now type the name (not including the domain) and password of an account that has permission to add the machine to the domain."
+$domaindns= Read-Host -Prompt "Domain FQDN ex; Contoso.com"
+$domacctdomain= Read-Host -Prompt "Shortname Domain, eg Contoso"
+$vm1 | Add-AzureProvisioningConfig -AdminUsername $cred1.GetNetworkCredential().Username -Password $cred1.GetNetworkCredential().Password -WindowsDomain -Domain $domacctdomain -DomainUserName $cred2.GetNetworkCredential().Username -DomainPassword $cred2.GetNetworkCredential().Password -JoinDomain $domaindns
+$vm1 | Set-AzureSubnet -SubnetNames "BackendSubnet"
+$disksize=20
+$disklabel="Database"
+$lun=0
+$hcaching="None"
+$vm1 | Add-AzureDataDisk -CreateNew -DiskSizeInGB $disksize -DiskLabel $disklabel -LUN $lun -HostCaching $hcaching
+$svcname="Ruxbin"
+$vnetname="Group InfrastructureCUSW-0 VNetCUSW-0"
+New-AzureVM –ServiceName $svcname -VMs $vm1 -VNetName $vnetname
